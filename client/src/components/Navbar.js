d5c8249361c8f34ef10ced8a45518d38bd2d27cc
Racheal Colbert <rachealcolbert16@gmail.com>
2021-03-20 13:06:17 -0700

    bootstrap styling added to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -16,17 +16,17 @@ const AppNavbar = () => {
       <Navbar bg="dark" variant="dark" expand="lg">
         <Container fluid>
           <Navbar.Brand as={Link} to="/">
-            Google Books Search
+            Movie Search
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbar" />
           <Navbar.Collapse id="navbar">
             <Nav className="ml-auto">
               <Nav.Link as={Link} to="/">
-                Search For Books
+                Search For Movies
               </Nav.Link>
               <>
                 <Nav.Link as={Link} to="/saved">
-                  See Your Books
+                  See Your Movies
                 </Nav.Link>
                 <Nav.Link onClick={logout}>Logout</Nav.Link>
               </>
