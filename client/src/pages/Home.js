a85358c2d5f060a1680d8410ae41a8f9351be58f
unknown <jonah.benson@gmail.com>
2021-03-23 22:25:06 -0700

    pull commit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,11 +9,9 @@ import {
   Container,
   Form,
 } from "react-bootstrap";
-import CardInfo from "../components/Card";
-
 import { searchMovies } from "../utils/API";
-import { useQuery } from "@apollo/client";
-import { GET_MOVIES } from "../utils/queries";
+// import { useQuery } from "@apollo/client";
+// import { GET_MOVIES } from "../utils/queries";
 
 const Home = () => {
   //   const { loading, data, error } = useQuery(GET_MOVIES);
@@ -40,7 +38,7 @@ const Home = () => {
       const movieData = movies.map((movie) => ({
         title: movie.Title,
         year: movie.Year,
-        image: movie.Poster
+        image: movie.Poster,
       }));
 
       setSearchedMovies(movieData);
@@ -73,33 +71,35 @@ const Home = () => {
         </div>
       </Jumbotron>
       <Container>
-        <h2>
+        {/* <h2>
           {searchedMovies.length
             ? `Viewing ${searchedMovies.length} results:`
             : ""}
-        </h2>
+        </h2> */}
         <CardColumns>
           {searchedMovies.map((movie) => {
             return (
-              <Card style={{ width: "18rem" }} key={movie.title} border="dark">
-                <Card.Img variant="top" src="" />
-                <Card.Body>
-                  <Card.Title>{movie.title}</Card.Title>
-<<<<<<< HEAD
-                  <Card.Text>
-                    <p> Year released: {movie.year}</p>
-                  </Card.Text>
-                  <Button variant="primary">See More</Button>
-=======
-
-                  <img src={movie.image} style={{ width: '100%' }} />
-                  <p> Year released: {movie.year}</p>
->>>>>>> 1d4b5d1f65fdb6f77c787f7f98c6abba7642ca97
-                </Card.Body>
-              </Card>
+              <Container>
+                <Card
+                style={{ width: "18rem" }}
+                key={movie.title}
+                >
+                  <Card.Img variant="top" src={movie.image} />
+                  <Card.Body>
+                    <Card.Title>
+                      {movie.title}
+                    </Card.Title>
+                    <Card.Text>
+                      Released in: {movie.year}
+                    </Card.Text>
+                    <Button variant="dark">Save to My Movies</Button>
+                  </Card.Body>
+                </Card>
+              </Container>
+              
             );
           })}
-          <pre>{JSON.stringify(data || null, null, 2)}</pre>
+          {/* <pre>{JSON.stringify(data || null, null, 2)}</pre> */}
         </CardColumns>
       </Container>
     </div>
